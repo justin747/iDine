593302b484ea51cc0b3b7d14ce0817cbff636511
Justin747 <justin747@gmail.com>
2022-01-02 15:30:33 -0800

    Initial Commit

---
+++ iDine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by Justin747 on 1/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Justin747 on 1/2/22.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
