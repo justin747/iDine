d7d320961956e7dcb2d7ae1428bceab0d69f771d
Justin747 <justin747@gmail.com>
2022-01-07 22:24:07 -0800

    Updated CheckoutView
    Added a Form to checkout view for Loyalty Points, Loyalty ID, and Tips

--- iDine/CheckoutView.swift
+++ iDine/CheckoutView.swift
@@ -11,18 +11,45 @@ struct CheckoutView: View {
     
     @EnvironmentObject var order: Order
     @State private var paymentType = "Cash"
+    @State private var addLoyaltyDetails = false
+    @State private var loyaltyNumber = ""
+    @State private var tipAmount = 15
     
     let paymentTypes = ["Cash", "Credit Card", "iDine Points"]
     
+    let tipAmounts = [10, 15, 20, 25, 0]
+    
     var body: some View {
-        VStack {
+        Form {
             Section {
                 Picker("How do you want to pay?", selection: $paymentType) {
                     ForEach(paymentTypes, id: \.self) {
                         Text($0)
                     }
                 }
+                
+                Toggle("Add iDine Loyalty Card", isOn: $addLoyaltyDetails.animation())
+                
+                if addLoyaltyDetails {
+                    TextField("Enter you iDine ID", text: $loyaltyNumber)
+                }
             }
+            
+            Section(header: Text("Add a tip?")) {
+                Picker("Percentage:", selection: $tipAmount) {
+                    ForEach(tipAmounts, id: \.self) {
+                        Text("\($0)%")
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+            }
+            
+            Section(header: Text("TOTAL: $100")) {
+                Button("Confirm Order") {
+                    //Place order
+                }
+            }
+            
         }
         .navigationTitle("Payment")
         .navigationBarTitleDisplayMode(.inline)
